731a57c64d2c4baee204c2d6fbe331a8ad5cf15f
Massimiliano Gallo <max.gallo@hailocab.com>
2016-10-12 17:41:40 +0100

    minor bug fix

--- chapter_10/quick-sort.js
+++ chapter_10/quick-sort.js
@@ -3,42 +3,47 @@
 Quick Sort
 */
 
-function quicksort(arr, left, right) {
-    var index = partition(arr, left, right);
-    if (left < index - 1) {
-        quicksort(arr, left, index - 1);
-    }
-    if (right > index) {
-        quicksort(arr, index, right);
-    }
+function quickSort(arr, left, right){
+  var index = partition(arr, left, right);    
+  
+  if(left < index - 1){
+    quickSort(arr, left, index - 1);
+  }
+  
+  if(right > index){
+    quickSort(arr, index + 1, right); 
+  }
+
 }
 
-function partition(arr, left, right) {
-    var pivot = arr[Math.floor((left + right) / 2)];
 
-    while (left < right) {
-        while (arr[left] < pivot) {
-            left++;
-        }
-        while (arr[right] > pivot) {
-            right--;
-        }
 
-        if (arr[left] > arr[right]) {
-            swap(arr, left, right);
-            left++;
-            right--;
-        }
+function partition(arr, left, right){
+    var pivot = arr[parseInt((left + right)/2)];
+
+    while(left < right){
+      while(arr[left] < pivot){
+        left++;  
+      }
+      while(arr[right] > pivot){
+        right--;  
+      }
+      if(arr[left] >= arr[right]){
+        swap(arr, left, right);
+        left++;
+        right--;
+      }
     }
-
-    return left + 1;
+  
+    return left;
 }
 
-function swap(arr, from, to) {
-    var temp = arr[to];
-    arr[to] = arr[from];
-    arr[from] = temp;
+function swap(arr, i, j){
+  var temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
+
 
 var arr = [3, 10, 5, 8, 2];
 console.log(arr);
